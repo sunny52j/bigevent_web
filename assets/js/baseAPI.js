3cdb9826bd1d80378b2f48410d7b80febccb089a
chenxi <1072524111@qq.com>
2021-07-16 17:52:48 +0800

    完成主页功能开发

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -3,5 +3,22 @@
 // 在这个函数中，可以拿到我们给Ajax提供的配置对象
 $.ajaxPrefilter(function(options){
     // 在发起真正的 Ajax请求之前，统─拼接请求的根路径
-    options.url='http://api-breakingnews-web.itheima.net'+options.url
+    options.url='http://api-breakingnews-web.itheima.net'+options.url;
+//统一为有权限的接口设置headers接口
+if(options.url.indexOf('/my/')!==-1){//url中包含/my/时
+    options.headers={
+        Authorization: localStorage.getItem('token') || ''
+    }
+}
+//全局统一挂载complete回调函数
+options.complete=function(res) {
+    console.log(res);
+    // 在complete回调函数中，可以使用 res.responseJSON拿到服务器响应回来的数据
+    if (res.responseJSON.status === 1 && res.responseJSON.message === '身份认证失败！') {
+        // 1.强制清空token
+        localStorage.removeItem('token');
+        //2.强制跳转到登录页面
+        location.href = '/login.html';
+    }
+}
 })
